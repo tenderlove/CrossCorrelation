af0916228b1e16ffb13ecfc2868fbea975501fb2
Ketron Mitchell-Wynne <ketronmw@users.noreply.github.com>
2016-07-01 12:12:19 -0700

    Delete randPosDiscrete.py

--- randPosDiscrete.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def randPosDiscrete(Nx, Ny, Ngal):
-    ''' 
-    Given a number of galaxies and the size of the array,
-    distribute them in a random, integral way.
-    '''
-    randx = np.random.random_integers(0, Nx-1, Ngal)
-    randy = np.random.random_integers(0, Ny-1, Ngal)
-    return (randx, randy)
